f4bf05031789519fdf8ee95ac47d4afa0d49e68f
lijing94 <lijing94@outlook.com>
2024-05-28 17:50:16 +0800

    fix

--- group.py
+++ group.py
@@ -1,4 +1,5 @@
 from global_val import *
+from numpy import *
 #按分组数进行分组
 def group_by_group_nums(group_num,per_group_mouse_num,*data_sorted):
     data_result = []
@@ -23,9 +24,25 @@ def group_by_group_nums_to_2dims_list(group_num,per_group_mouse_num,*data_sorted
         data_result_tmp = []
         for j in range(0,per_group_mouse_num):
             num = i+j*group_num
-            data_result_tmp.append(data_sorted[num])
+            print(data_sorted[num])
+            data_result_tmp.append(data_sorted[num][4])
         data_result.append(data_result_tmp)
-    #print("data_result:"+str(len(data_result)))
     print_format_excel(*data_result)
-    
+    avg_row_calc_for_2dim_list(*data_result)
     return data_result
+
+def avg_row_calc_for_2dim_list(*data_result):
+    data_result_tmp = []
+    m_avg = []
+    for i in data_result:
+        m_row = i
+        sum = 0
+        count = 0
+        i.append(round(mean(i),2))
+        data_result_tmp.append(i)
+        for m in m_row:
+            sum = sum + m
+            count = count + 1
+        print(data_result_tmp)
+    print_format_excel(*data_result_tmp)
+
