1e5a8a5d1ef770d908c0ef944dee5a1ca466a269
lijing94 <lijing94@outlook.com>
2024-05-28 17:51:32 +0800

    fix

--- group.py
+++ group.py
@@ -20,29 +20,26 @@ def group_by_group_nums(group_num,per_group_mouse_num,*data_sorted):
 def group_by_group_nums_to_2dims_list(group_num,per_group_mouse_num,*data_sorted):
     data_result = []
     data_result_tmp = []
-    for i in range(0,group_num):
+    for i in range(0,per_group_mouse_num): #colums
         data_result_tmp = []
-        for j in range(0,per_group_mouse_num):
-            num = i+j*group_num
+        for j in range(0,group_num):     #rows
+            num = j+i*per_group_mouse_num
             print(data_sorted[num])
             data_result_tmp.append(data_sorted[num][4])
         data_result.append(data_result_tmp)
     print_format_excel(*data_result)
-    avg_row_calc_for_2dim_list(*data_result)
+    avg_row_calc_for_2dim_list(group_num,*data_result)
     return data_result
 
-def avg_row_calc_for_2dim_list(*data_result):
+def avg_row_calc_for_2dim_list(group_num,*data_result):
     data_result_tmp = []
-    m_avg = []
+    row_avg_2_float = []
     for i in data_result:
-        m_row = i
-        sum = 0
-        count = 0
-        i.append(round(mean(i),2))
         data_result_tmp.append(i)
-        for m in m_row:
-            sum = sum + m
-            count = count + 1
-        print(data_result_tmp)
+        m_round = mean(data_result,axis=0)
+    for i in m_round:
+        n = round(i,2)
+        row_avg_2_float.append(n)
+    data_result_tmp.append(row_avg_2_float)
     print_format_excel(*data_result_tmp)
 
